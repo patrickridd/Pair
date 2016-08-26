c28f90c7c0b89a272a78aeadd2384dbe84d33096
pattyhackins <patrickridd@yahoo.com>
2016-08-26 10:47:54 -0600

    Can save and fetch entities from context.

--- Pair/EntitiesTableViewController.swift
+++ Pair/EntitiesTableViewController.swift
@@ -7,39 +7,116 @@
 //
 
 import UIKit
+import CoreData
 
-class EntitiesTableViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+class EntitiesTableViewController: UIViewController, UITableViewDataSource,UITableViewDelegate, NSFetchedResultsControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        EntityController.sharedController.fetchedResultsController.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
   
     @IBAction func randomizeButtonTapped(sender: AnyObject) {
+        
+        
     }
     
     @IBAction func addButtonTapped(sender: AnyObject) {
+        var  nameTextField = UITextField()
+        
+        let alert = UIAlertController(title: "What is the 'Name' for Entity", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "enter name..."
+            nameTextField = textField
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        let submitAction = UIAlertAction(title: "Submit", style: .Default) { (_) in
+            guard let name = nameTextField.text where !(name.isEmpty) else {
+                return
+            }
+            EntityController.sharedController.addEntity(name)
+            
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(submitAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+        
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        guard let sections =  EntityController.sharedController.fetchedResultsController.sections?.count else {
+            return 1
+        }
+        return sections
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        guard let sections =  EntityController.sharedController.fetchedResultsController.sections else {
+            return EntityController.sharedController.fetchedResultsController.fetchedObjects?.count ?? 0
+        }
+
+        return sections[section].numberOfObjects
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("entityCell", forIndexPath: indexPath)
-        
-        
+        let entity =  EntityController.sharedController.fetchedResultsController.objectAtIndexPath(indexPath)
+
+        cell.textLabel?.text = entity.name
         
         return cell
     }
     
+    
+    // MARK: - NSFetchedResultsController Delegate Methods
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
+    }
+    
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        
+        switch type {
+        case .Delete:
+            tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Automatic)
+        case .Insert:
+            tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Automatic)
+        default: break
+        }
+
+    }
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+       
+        switch type {
+        case .Delete:
+            guard let indexPath = indexPath else { return }
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        case .Insert:
+            guard let newIndexPath = newIndexPath else { return }
+            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Automatic)
+        case .Update:
+            guard let indexPath = indexPath else { return }
+            
+            tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        case .Move:
+            guard let indexPath = indexPath, newIndexPath = newIndexPath else { return }
+            
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Automatic)
+        }
+
+        
+        
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.endUpdates()
+    }
     
     /*
     // MARK: - Navigation
--- Pair/Pair/EntityController.swift
+++ Pair/Pair/EntityController.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import CoreData
+
+class EntityController {
+
+    static let sharedController = EntityController()
+    let moc = Stack.sharedStack.managedObjectContext
+    
+    var entities = [Entity]()
+    
+    let fetchedResultsController: NSFetchedResultsController
+    
+    init() {
+        
+        let request = NSFetchRequest(entityName: "Entity")
+        request.sortDescriptors = []
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        
+        _ = try? fetchedResultsController.performFetch()
+    }
+    
+    func addEntity(name: String) {
+        let _ = Entity(name: name)
+        saveContext()
+    }
+    
+    
+    func saveContext() {
+        do {
+            try moc.save()
+        } catch let error as NSError {
+            print("Couldn't save to Context. Error: \(error.localizedDescription)")
+        }
+        
+            }
+    
+    func fetchContext() {
+        
+    }
+    
+    
+}
